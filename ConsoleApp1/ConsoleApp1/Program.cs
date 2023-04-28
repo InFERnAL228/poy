37a494326439b6a60296ee5832f00b821f75eeca
Shiklin Semyon <typokabra777@gmail.com>
2023-04-28 17:11:55 +0300

    Настроил репозиторий

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main()
+        {
+            double m = 0.0;
+            double cm = 0.0;
+            double mm = 0.0;
+            double d = 0.0;
+            Console.WriteLine("Введите число дюймов:");
+            d = Convert.ToDouble(Console.ReadLine()); //
+            cm = 2.54 * d;
+            mm = 2.54 * d * 10;
+            m = 2.54 * d / 100; //
+            Console.WriteLine(" {0} м, {1} см, {2} мм", m, cm, mm);
+            Console.ReadKey();
+        }
+    }
+}
