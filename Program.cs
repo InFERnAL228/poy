df9b87df7d8dadcc2f3771f20eb7237f731f8dee
xoy <89229642303@mail.ru>
2023-05-23 14:28:54 +0300

    Создание и решение конфликта

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;//Библиотека
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace ConsoleApp2
+{
+	class Program//Класс
+	{
+		static void Main()//Метод Main //
+		{
+			double m;//Объявляем переменную
+			double cm;
+			double mm; //
+			double d;
+			Console.WriteLine("Введите число дюймов:");//Вводим данные в консоль
+			d = Convert.ToDouble(Console.ReadLine()); //Конвертируем
+			cm = 2.54 * d;//Находим см
+			mm = 2.54 * d * 10;//Находим мм
+			m = 2.54 * d / 100; //Находим м
+			Console.WriteLine(" {0} м, {1} см, {2} мм", m, cm, mm);//Выоводим результат
+			Console.ReadKey();//Задержка экрана //
+		}
+	}
+}
